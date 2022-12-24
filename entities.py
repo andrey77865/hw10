309f8a53dd7dde3c4196a7bdaa0a1ed2b973733b
Andrey K <andrey77865@proton.me>
2022-12-24 21:56:25 +0200

    Create entities.py
    Adds classes without functionality

---
+++ entities.py
@@ -0,0 +1,21 @@
+from collections import UserDict
+
+
+class AddressBook(UserDict):
+    pass
+
+
+class Record:
+    pass
+
+
+class Field:
+    pass
+
+
+class Name:
+    pass
+
+
+class Phone:
+    pass
