ff974d80774d9d5e1f852beca7438218a4ffb526
Andrey K <andrey77865@proton.me>
2022-12-28 15:28:08 +0200

    final commit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from decorators import two_commands_func, one_command_func
-
+from entities import AddressBook, Record, Field, Name, Phone
 
 CONTACTS = {}
 
@@ -109,4 +109,16 @@ def handler_app():
 
 
 if __name__ == "__main__":
-    handler_app()
+    name = Name('Bill')
+    phone = Phone('1234567890')
+    rec = Record(name, phone)
+    ab = AddressBook()
+    ab.add_record(rec)
+
+    assert isinstance(ab['Bill'], Record)
+    assert isinstance(ab['Bill'].name, Name)
+    assert isinstance(ab['Bill'].phones, list)
+    assert isinstance(ab['Bill'].phones[0], Phone)
+    assert ab['Bill'].phones[0].value == '1234567890'
+
+    print('All Ok)')
